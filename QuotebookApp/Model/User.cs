a5005efc13a2af7289556ecfa1e38884afdc6f18
Evan Armoogan <evanjarmoogan@gmail.com>
2023-12-30 11:54:01 -0500

    Added app administrators and invisible users. App admins don't have any extra permissions (yet), but invisible users do not pop up in any pickers (filter quotes, add quote, etc.) and they do not have the ability to add quotes

--- QuotebookApp/Model/User.cs
+++ QuotebookApp/Model/User.cs
@@ -2,14 +2,23 @@
 
 public class User
 {
-    public User(int userID, string userName, string userPass)
+    public enum UserPermissionType
+    {
+        Admin = 0,
+        Normal,
+        Invisible,
+    }
+
+    public User(int userID, string userName, string userPass, UserPermissionType userType)
     {
         UserID = userID;
         UserName = userName;
         UserPass = userPass;
+        UserType = userType;
     }
 
     public int UserID { get; set; }
     public string UserName { get; set; }
     public string UserPass { get; set; }
+    public UserPermissionType UserType { get; set; }
 }
